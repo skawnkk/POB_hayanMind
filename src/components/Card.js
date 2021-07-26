f490a35662bd6ea846fa4494cf6c393005538f27
Namju <skawn11114@gmail.com>
2021-07-27 02:54:23 +0900

    [#3] style : 카드 컴포넌트 생성 및 스타일 적용
    close : #3

---
+++ src/components/Card.js
@@ -0,0 +1,46 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const Card = ({ content }) => {
+  const { id, email, body } = content;
+  return (
+    <CardWrapper>
+      <CardTitle>
+        Comment Id<span>{id}</span>
+      </CardTitle>
+
+      <CardTitle>
+        Email<span>{email}</span>
+      </CardTitle>
+
+      <CardTitle>Comment</CardTitle>
+      <p>{body}</p>
+    </CardWrapper>
+  );
+};
+
+export default Card;
+
+const CardWrapper = styled.div`
+  width: 500px;
+  margin-bottom: 12px;
+  padding: 20px;
+  border: 0.5px solid ${({ theme }) => theme.color.borderline};
+  border-radius: 20px;
+  background-color: ${({ theme }) => theme.color.background};
+`;
+
+const CardTitle = styled.div`
+  font-weight: 800;
+  margin: 12px 0;
+
+  &:nth-last-child(2) {
+    margin-bottom: 2px;
+  }
+
+  span {
+    display: inline-block;
+    margin-left: 12px;
+    font-weight: 400;
+  }
+`;
