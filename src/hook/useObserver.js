3cbc83506fa51faa3787dd3a08e53f77958961a1
Namju <skawn11114@gmail.com>
2021-07-28 05:20:14 +0900

    [#5] feat : intersection Observer hook구현

---
+++ src/hook/useObserver.js
@@ -0,0 +1,22 @@
+import { useEffect, useState } from 'react';
+export const useObserver = (target) => {
+  const defaultOptions = {
+    rootMargin: '600px',
+    threshold: 0.5,
+  };
+  const [isIntersecting, setIntersecting] = useState(false);
+  const observer = new IntersectionObserver((entries) => {
+    setIntersecting(entries.isIntersecting);
+  }, defaultOptions);
+
+  useEffect(() => {
+    if (target?.current) {
+      observer.observe(target.current);
+    }
+    return () => {
+      observer.disconnect();
+    };
+  }, [observer, target]);
+
+  return isIntersecting;
+};
